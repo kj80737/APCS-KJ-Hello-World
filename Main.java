8f37d5c8c2d97b8f8ca987aba859f1ebe223c9d7
kj80737 <69651311+kj80737@users.noreply.github.com>
2020-08-14 13:03:51 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Systen.out.println("Hello World");
+		System.out.println("My name is Kelvin Jiang");
+		System.out.println("Hobbies: Reading");
+		System.out.println("Favorite video game: League of Legends");
+		System.out.println("College: Any");
+		System.out.println("Major: Computer Science");
+		
+	}
+
+}
